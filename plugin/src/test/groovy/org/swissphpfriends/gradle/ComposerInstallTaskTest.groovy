873d7228e3e6449c78f36542f06db75b2283d26c
frne <frank.neff@ymc.ch>
2014-08-15 08:24:14 +0200

    More tests and cleaned up buildscript

---
+++ plugin/src/test/groovy/org/swissphpfriends/gradle/ComposerInstallTaskTest.groovy
@@ -0,0 +1,16 @@
+package org.swissphpfriends.gradle
+
+import org.junit.Test
+import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.api.Project
+import static org.junit.Assert.*
+
+class ComposerInstallTaskTest {
+
+    @Test
+    public void canAddTaskToProject() {
+        Project project = ProjectBuilder.builder().build()
+        def task = project.task('composerInstall', type: ComposerInstallTask)
+        assertTrue(task instanceof ComposerInstallTask)
+    }
+}
