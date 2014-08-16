cc458cb3a447f391c4cdfb2d2e98dded537ac0be
frne <fneff89@gmail.com>
2014-08-16 10:55:39 +0200

    Added doNotUpdatePhar option to ComposerInstall task

--- plugin/src/main/groovy/org/swissphpfriends/gradle/task/ComposerInstall.groovy
+++ plugin/src/main/groovy/org/swissphpfriends/gradle/task/ComposerInstall.groovy
@@ -5,15 +5,17 @@ import org.swissphpfriends.gradle.AbstractBaseTask
 
 class ComposerInstall extends AbstractBaseTask {
 
+    Boolean doNotUpdatePhar = false
+
     @TaskAction
-    def composerInstall() {
+    def run() {
 
         this.checkPhpInstallation();
 
         if (!this.isAvailable()) {
             this.installPhar();
 
-        } else {
+        } else if (this.doNotUpdatePhar.equals(false)) {
             this.checkForSelfUpdate();
         }
 
@@ -21,6 +23,7 @@ class ComposerInstall extends AbstractBaseTask {
         this.printBuildMessage('installing dependencies using composer...');
 
         Process composerInstallProcess = new ProcessBuilder("php", "composer.phar", "install")
+                .directory(new File(this.workingDirectory))
                 .redirectErrorStream(true)
                 .start()
         composerInstallProcess.inputStream.eachLine { println it }
@@ -32,6 +35,7 @@ class ComposerInstall extends AbstractBaseTask {
      */
     private boolean isAvailable() {
         def composerVersionProcess = new ProcessBuilder('php', 'composer.phar', '--version')
+                .directory(new File(this.workingDirectory))
                 .redirectErrorStream(true)
                 .start()
 
@@ -49,13 +53,15 @@ class ComposerInstall extends AbstractBaseTask {
 
         // download installer
         def address = 'https://getcomposer.org/installer'
-        def file = new FileOutputStream(address.tokenize("/")[-1])
+        def path = this.workingDirectory + '/' + address.tokenize("/")[-1]
+        def file = new FileOutputStream(path)
         def out = new BufferedOutputStream(file)
         out << new URL(address).openStream()
         out.close()
 
         // install composer.phar
         def installComposerProcess = new ProcessBuilder('php', './installer')
+                .directory(new File(this.workingDirectory))
                 .redirectErrorStream(true)
                 .start()
 
@@ -65,12 +71,16 @@ class ComposerInstall extends AbstractBaseTask {
         if (installComposerProcess.exitValue()) {
             println "Composer installation failed!"
         }
+
+        File installer = new File(path);
+        installer.delete();
     }
 
     private void checkForSelfUpdate() {
         this.printBuildMessage('composer.phar already present. Checking for update...');
 
         Process composerSelfupdateProcess = new ProcessBuilder("php", "composer.phar", "selfupdate")
+                .directory(new File(this.workingDirectory))
                 .redirectErrorStream(true)
                 .start()
         composerSelfupdateProcess.inputStream.eachLine { println it }
--- plugin/src/test/groovy/org/swissphpfriends/gradle/task/ComposerInstallTest.groovy
+++ plugin/src/test/groovy/org/swissphpfriends/gradle/task/ComposerInstallTest.groovy
@@ -1,9 +1,9 @@
 package org.swissphpfriends.gradle.task
 
+import org.gradle.api.Task
 import org.junit.Test
 import org.gradle.testfixtures.ProjectBuilder
 import org.gradle.api.Project
-import org.swissphpfriends.gradle.task.ComposerInstall
 
 import static org.junit.Assert.*
 
@@ -14,5 +14,34 @@ class ComposerInstallTest {
         Project project = ProjectBuilder.builder().build()
         def task = project.task('composerInstall', type: ComposerInstall)
         assertTrue(task instanceof ComposerInstall)
+    }
+
+    @Test
+    public void installsComposerPharAndDependencies() {
+        Task composerInstallTask = this.getTaskInstance();
+
+        assertTrue(composerInstallTask.hasProperty('workingDirectory'))
+        assertEquals('./',composerInstallTask.getProperty('workingDirectory'))
+        composerInstallTask.setProperty('workingDirectory', '../user', );
+        assertEquals('../user',composerInstallTask.getProperty('workingDirectory'))
+
+        composerInstallTask.run();
+
+        File composerBinary = new File('../user/composer.phar');
+        assertTrue(composerBinary.exists());
+        assertTrue(composerBinary.canExecute());
+
+        File vendorDir = new File('../user/vendor');
+        assertTrue(vendorDir.exists());
+        assertTrue(vendorDir.isDirectory());
+        assertTrue(vendorDir.canRead());
+
+        File composerInstaller = new File('../user/installer');
+        assertFalse(composerInstaller.exists());
+    }
+
+    private Task getTaskInstance() {
+        Project project = ProjectBuilder.builder().build()
+        return project.task('phpunit', type: ComposerInstall)
     }
 }
